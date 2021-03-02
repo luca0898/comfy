493fbd7fc11368d0d5dedc5e834f4f62ce0c6fae
luca <luca@apinteractive.com.br>
2021-03-02 17:30:21 -0300

    feat: create a new jwt bearer authorize attribute

---
+++ backend/Comfy.SystemObjects/Attributes/BearerAuthorizeAttribute.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Comfy.SystemObjects.Attributes
+{
+    public class BearerAuthorizeAttribute : AuthorizeAttribute
+    {
+        public BearerAuthorizeAttribute(string policy)
+        {
+            Policy = policy;
+            AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme;
+        }
+    }
+}
