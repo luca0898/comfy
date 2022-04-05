b290e800f15e4d7a1fc05e0d766a270cff585091
luca0898 <lucaguiraldello@hotmail.com>
2022-04-05 20:48:57 -0300

    atualizando versões do projeto frontend

--- webapp/src/routes/index.tsx
+++ webapp/src/routes/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Routes as Switch, Route } from 'react-router-dom';
 import DashboardScreen from '../pages/Dashboard/Dashboard';
 import LoginScreen from '../pages/LoginScreen';
 
 const Routes : React.FC = () => (
   <Switch>
-    <Route path="/login" component={LoginScreen} exact />
-    <Route path="/" component={DashboardScreen} exact />
+    <Route path="/login" element={<LoginScreen />} caseSensitive />
+    <Route path="/" element={<DashboardScreen />} caseSensitive />
   </Switch>
 );
 
